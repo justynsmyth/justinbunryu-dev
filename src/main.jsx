7b82390e1cd4090f331fe201d44703a081518d88
jbusmith <jbusmith@umich.edu>
2025-10-04 18:12:17 -0400

    Removed basename

--- src/main.jsx
+++ src/main.jsx
@@ -15,19 +15,16 @@ import Header from './components/Header';
 import Home from './routes/Home';
 import Experience from './routes/Experience';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Header />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: 'experience', element: <Experience /> },
-      ],
-    },
-  ],
-  { basename: '/portfolio-react' } // This is to inform Github-pages to use a basepath as an absolute starting position in Github URL
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'experience', element: <Experience /> },
+    ],
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
