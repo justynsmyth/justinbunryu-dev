11eac15f5ed3f0f021baedd0fdd8a5bd62dbd02f
jbusmith <jbusmith@umich.edu>
2025-10-04 18:01:46 -0400

    Fix issue where GitHub Pages relative starting page makes 404 errors

--- src/main.jsx
+++ src/main.jsx
@@ -14,16 +14,19 @@ import Header from './components/Header';
 import Home from './routes/Home';
 import Experience from './routes/Experience';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Header />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'experience', element: <Experience /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Header />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'experience', element: <Experience /> },
+      ],
+    },
+  ],
+  { basename: '/portfolio-react' } // This is to inform Github-pages to use a basepath as an absolute starting position in Github URL
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
